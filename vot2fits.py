556fef4a3fef04115878c8fa21ac63c3247e967d
Alec Thomson (S&A, Kensington WA) <alec.thomson@csiro.au>
2023-10-25 16:36:31 +0800

    Clean up

--- vot2fits.py
+++ vot2fits.py
@@ -1,15 +1,17 @@
 #!/usr/bin/env python3
 """Convert VOTable to FITS"""
+from pathlib import Path
+from typing import List
+
 import click
 from astropy.table import Table
-from typing import List
-from pathlib import Path
 
 VOT_EXTENSIONS = (
     ".vot",
     ".xml",
     ".votable",
 )
+
 
 @click.command()
 @click.argument("file_names", nargs=-1)
@@ -20,10 +22,9 @@ VOT_EXTENSIONS = (
     help="Overwrite existing files",
 )
 def main(
-        file_names: List[str],
-        overwrite: bool = False,
+    file_names: List[str],
+    overwrite: bool = False,
 ):
-    
     for file_name in file_names:
         file_path = Path(file_name)
         if file_path.suffix not in VOT_EXTENSIONS:
@@ -32,11 +33,6 @@ def main(
         table = Table.read(file_name)
         fits_name = file_path.with_suffix(".fits")
         table.write(fits_name, overwrite=overwrite)
-
-
-
-def cli(file_names, overwrite):
-    main(file_names, overwrite=overwrite)
 
 
 if __name__ == "__main__":
